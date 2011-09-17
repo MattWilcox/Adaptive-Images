5465e61da9a7accb1254f3c888755e42c0cdc061
Oncle Tom <thomas@oncle-tom.net>
2011-09-17 10:57:47 +0300

    * replying with no content (only headers are sent; fast for client side) * fixing a possible cookie injection

--- ai-cookie.php
+++ ai-cookie.php
@@ -1,9 +1,9 @@
 <?php
-$maxwidth = $_GET['maxwidth'];
-if($maxwidth == "unknown") { $maxwidth = 3000; } // we need a number, so give it something unfeasable
+$maxwidth = isset($_GET['maxwidth']) ? intval($_GET['maxwidth']) : null;
+
+if(null === $maxwidth || $maxwidth === "unknown") { $maxwidth = 3000; } // we need a number, so give it something unfeasable
 setcookie('resolution',$maxwidth,time()+604800,'/'); // set the cookie
 
-// respond with a (fake) blank image
-header('content-type: image/jpeg');
+// respond with an empty content
+header('HTTP/1.1 204 No Content');
 exit();
-?>

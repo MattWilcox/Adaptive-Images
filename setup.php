69e74fcaf76ceaa55096db7b1aac76bf853d8c16
johannheyne <mail@johannheyne.de>
2012-05-18 08:26:02 +0200

    NEW setup file for sharpen and imagebreackpoints

---
+++ setup.php
@@ -0,0 +1,9 @@
+<?php
+	
+	//$config['sharpen']['amount'] = '80'; // max 500
+	
+	//$setup['test']['breackpoints']['default'] = '100%';
+	//$setup['test']['breackpoints']['small'] = '200px';
+	//$setup['test']['ratio'] = '4:3';
+	
+?>

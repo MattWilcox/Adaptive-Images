9f1e88143f0f6a387ac914bb95fe10767d0239e0
Darkwing371 <darkwing371@gmx.de>
2013-04-24 23:10:00 +0200

    First commit for GitHub.

--- adaptive-images-uber-extended/adaptive-images-cookie-line.js
+++
@@ -1,8 +0,0 @@
-// Original by Matt Wilcox; (included for legacy reasons)
-// document.cookie='resolution='+Math.max(screen.width,screen.height)+'; path=/';
-
-// Original by Matt Wilcox, introducing device pixel ratio; (it is our line of choice)
-document.cookie = 'resolution=' + Math.max( screen.width, screen.height ) + ( "devicePixelRatio" in window ? "," + devicePixelRatio : ",1") + '; path=/';
-
-// Enable this just for testing purposes! Use it to fake device width and pixel density ratio.
-// document.cookie = 'resolution=1024,1' + '; path=/';
